a524172ed5ce2d2d04ea10ff01c5bcaa2c9eb925
zhangzicheng <zhangzicheng@naolubrain.com>
2023-07-28 10:10:37 +0800

    Upgrade kotlin version to 1.8.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "1.8.22"
     application
 }
 
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation(project(":library"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.2")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")//最新版本不兼容Android4.4
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
